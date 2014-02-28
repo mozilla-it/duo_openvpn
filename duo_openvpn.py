5e8e2edb38357956b97a7a51337ea3ed6559374d
Guillaume Destuynder <kang@mozilla.com>
2014-02-28 14:54:09 -0800

    Add vim modeline because TABS! :)

--- duo_openvpn.py
+++ duo_openvpn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# vim: set noexpandtab:ts=4
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this

ad2c6090b327b6829693d3fa5e246bc30b434fba
Guillaume Destuynder <kang@mozilla.com>
2016-01-26 15:59:36 -0800

    Add pip, fpm support

--- Makefile
+++ Makefile
@@ -1,11 +1,28 @@
-CC		:= gcc
+CC	:= gcc
 CFLAGS	:=
 LDFLAGS	:= -fPIC -shared
 INSTALL	:= install
 DESTDIR	:= /
 PREFIX	:= /usr
+PACKAGE := duo_openvpn
+VERSION := 1.0.0
 
 all: plugin
+	./setup.py build
+
+pyinstall:
+	./setup.py install
+
+rpm:
+	$(MAKE) DESTDIR=./tmp install
+	fpm -s dir -t rpm -d mozdef_client -n $(PACKAGE) -v $(VERSION) -C tmp
+
+deb:
+	$(MAKE) DESTDIR=./tmp install
+	fpm -s dir -t deb -d mozdef_client -n $(PACKAGE) -v $(VERSION) -C tmp
+
+pypi:
+	python setup.py sdist check upload --sign
 
 plugin: duo_openvpn.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -I. -c duo_openvpn.c
@@ -24,3 +41,8 @@ clean:
 	rm -f *.so
 	rm -f *.pyc
 	rm -rf __pycache__
+	rm -rf dist sdist build
+	rm -rf $(PACKAGE).egg-info
+	rm -rf tmp
+	rm -rf *.rpm
+	rm -rf *.deb
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright (c) 2016 Mozilla Corporation
+# Author: gdestuynder@mozilla.com
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "duo_openvpn",
+        py_modules = ['duo_openvpn'],
+        version = "1.0.0",
+        author = "Guillaume Destuynder",
+        author_email = "gdestuynder@mozilla.com",
+        description = ("A plugin to OpenVPN to use DuoSecurity and LDAP authentication"),
+        license = "MPL",
+        keywords = "mozdef duosecurity openvpn",
+        url = "https://github.com/mozilla-it/duo_openvpn",
+        long_description = read('README.rst'),
+        requires = ['mozdef_client'],
+        classifiers = [
+            "Development Status :: 5 - Production/Stable",
+            "Topic :: System :: Logging",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        ],
+)
